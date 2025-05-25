ac54734b6d343e7473859e7fc31eb2412c88d1ed
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-05-26 01:23:14 +0530

    Add initial TypeScript and JavaScript files with variable declarations and console logs

---
+++ 01TS/01.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "varun";
+console.log(name.length); // 5
+console.log(name); // varun
+var num1 = "6";
+console.log(num1); // 6 (string type)
+num1 = "helo";
+console.log(num1); // helo (string type)
---
+++ 01TS/01.ts
@@ -0,0 +1,44 @@
+let name: string = "varun";
+console.log(name.length); // 5
+console.log(name); // varun
+
+let num1: string = "6";
+console.log(num1); // 6 (string type)
+num1 = "helo";
+console.log(num1); // helo (string type)
+
+// let num2: string = "6";
+// num2 = 2;
+// console.log(num2); 
+// error due to conflict in data types that we store in num2
+
+// let b: boolean=;
+// b = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export{}
